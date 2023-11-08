3c97dd8ac84b887e18fd5dd2fb0c4ea378a2d6af
abhishek <prashanpandey36@gmail.com>
2023-11-08 15:37:53 +0530

    model changes successfully

--- users/models.py
+++ users/models.py
@@ -6,6 +6,8 @@ class User(AbstractUser):
     name=models.CharField(max_length=255)
     email=models.CharField(max_length=255, unique=True)
     password=models.CharField(max_length=255)
+    mobile=models.CharField(max_length=10,default="")
+    otp=models.BooleanField(default=False)
     username=None
     
     USERNAME_FIELD='email'
